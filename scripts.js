5b0b5f8a78733ed74ce4f8adc21bb5b6aa00aad4
ruancs <ruan.cs@hotmail.com>
2020-06-27 19:47:34 -0300

    subindo atualizações JS

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+$('.single-item').slick({
+ dots:true,
+ arrows:true,
+ infinite: false,
+ slidesToShow: 1,
+ slidesToScroll: 1
+});
+
+ $('.multiple-items').slick({
+ dots:true,
+ arrows:true,
+ infinite: false,
+ slidesToShow: 4,
+ slidesToScroll: 4
+});
